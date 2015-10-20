d095b80582f3777f3fabfb03f5e45637931fadf8
Surfraz Ahmed <surfraz@gmail.com>
2015-10-20 08:14:06 +0100

    PP-186 added support for ELASTICSEARCH_INDEX env var

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pay-logger
+
+A simple syslog to AWS ES log forwarder.
+
+Runs on a docker host and listens for Syslog on port 5514/tcp.
+
+Uses the ELASTICSEARCH_HOST and ELASTICSEARCH_INDEX env variable and forwards any received syslog logs to this host.
+
+Designed to be used with the docker syslog logging driver.
--- run-syslog-ng.sh
+++ run-syslog-ng.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-env
-cat /etc/syslog-ng/syslog-ng.conf
+
+# Update syslog config with env from docker run
 sed -i "s/ELASTICSEARCH_HOST/${ELASTICSEARCH_HOST}/" /etc/syslog-ng/syslog-ng.conf
+sed -i "s/ELASTICSEARCH_INDEX/${ELASTICSEARCH_INDEX}/" /etc/syslog-ng/syslog-ng.conf
+
+# show the config for debugging purposes
 cat /etc/syslog-ng/syslog-ng.conf
+
 syslog-ng --no-caps -Fdv
